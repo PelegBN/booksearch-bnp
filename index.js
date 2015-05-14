64b3e0f09e68d11b3e60de772e57920163717a2b
PelegBN <pelegbn@gmail.com>
2015-05-14 23:23:39 +0300

    res.json

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ var app = express();
 app.get('/getAllBooks' ,function(req,res) {
 	console.log('"getAllBooks" initiated');
 	var list = books.getAllBooks();
-	res.end(list);
+	res.json(list);
 });
 
 app.get('/getBook/:id' ,function(req,res) {
 	console.log('"getBook" initiated with the value ' + req.params.id);
 	var book = books.getBook(req.params.id);
-	res.end(book);
+	res.json(book);
 });
 
 app.get('/getType/:type' ,function(req,res) {
 	console.log('"getType" initiated with the value ' + req.params.type);
 	var bookType = books.getType(req.params.type);
-	res.end(bookType);
+	res.json(bookType);
 });
 
 app.listen(process.env.PORT || 3000);
--- myModules/index.js
+++ myModules/index.js
@@ -1,20 +1,20 @@
 var bookList = [
-{id:"1", name:"a", author:"aa", year:"2015", type:"Comedy", price:"11", rate:"1"},
-{id:"2", name:"b", author:"bb", year:"2015", type:"Drama", price:"22", rate:"2"},
-{id:"3", name:"c", author:"cc", year:"2015", type:"Drama", price:"33", rate:"3"},
-{id:"4", name:"d", author:"dd", year:"2015", type:"Drama", price:"44", rate:"4"},
-{id:"5", name:"e", author:"ee", year:"2015", type:"Comedy", price:"55", rate:"5"},
-{id:"6", name:"f", author:"ff", year:"2015", type:"Comedy", price:"66", rate:"6"}
+{id:"1", name:"Me and Shmerling", author:"BNP", year:"1991", type:"Comedy", price:"79$"},
+{id:"2", name:"Down memory lane", author:"Peleg", year:"1982", type:"Drama", price:"52$"},
+{id:"3", name:"Summer Time At Nowhere", author:"PelegBN", year:"1987", type:"Youth", price:"45$"},
+{id:"4", name:"Making Friends", author:"BNP", year:"2013", type:"Drama", price:"44$"},
+{id:"5", name:"Fluffy McFluff", author:"Natalie", year:"2010", type:"Children", price:"35$"},
+{id:"6", name:"Lillian Made a Mess", author:"Tuti", year:"2002", type:"Thriller", price:"35$"}
 ];
 
 exports.getAllBooks = function () {
-	return JSON.stringify(bookList);
+	return bookList;
 }
 
 exports.getBook = function (id) {
 	for (var i=0 ; i < bookList.length ; i++)
 	{
-		if (bookList[i].id == id) return JSON.stringify(bookList[i]);
+		if (bookList[i].id == id) return bookList[i];
 	}
 }
 
@@ -27,6 +27,6 @@ exports.getType = function (type) {
 			typeList.push(bookList[i]);
 		} 	
 	}
-	if (typeList.length > 0) return JSON.stringify(typeList);
+	if (typeList.length > 0) return typeList;
 	else return "No Books by That Type Were Found";
 }
